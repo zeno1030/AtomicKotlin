585c71ba91fdc6b7a1c759a199be5f7477dba7c5
Kwon Ji Hwan <zeno1030@naver.com>
2025-02-18 15:18:40 +0900

    Initial commit

---
+++ Programming Basics/var   val/Exercise 3/src/Task.kt
@@ -0,0 +1,12 @@
+// VarAndVal/Task3.kt
+package varAndValExercise3
+
+fun main() {
+  var x = 1
+  var y = 2
+  val temp = x
+  x = y
+  y = temp
+  println(x)
+  println(y)
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AtomicKotlin
+ 아토믹 코틀린 실습 공간
